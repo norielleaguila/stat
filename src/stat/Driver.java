d5170a3d27e509511a65f34e5447ac2ea30c3ad2
Norielle Aguila <norielle_aguila@dlsu.edu.ph>
2017-12-14 02:08:37 +0800

    with dummy variables for publisher and platform

--- src/stat/Driver.java
+++ src/stat/Driver.java
@@ -10,22 +10,40 @@ public class Driver {
 
 	public static void main(String[] args) {
 		try {
-			Scanner sc = new Scanner(new File("out.csv"));
-			PrintWriter pw = new PrintWriter("top2016.csv");
+			Scanner sc = new Scanner(new File("top2016.csv"));
+			PrintWriter pw = new PrintWriter("top2016-fix.csv");
 			
-//			ArrayList<String> data = new ArrayList<>();
 			pw.println(sc.nextLine());
+			
 			while(sc.hasNextLine()){
 				String str = sc.nextLine();
-				if(str.contains(",2016,")){
-					if(str.contains("Square Enix") || 
-							str.contains("Nintendo") ||
-							str.contains("Sony Computer Entertainment") ||
-							str.contains("Capcom") ||
-							str.contains("Electronic Arts")){
-						pw.println(str);
-					}
-				}
+				if(str.contains("Electronic Arts"))
+					str += ",0,0,0,0";
+				else if(str.contains("Square Enix"))
+					str += ",0,0,0,1";
+				else if(str.contains("Nintendo"))
+					str += ",0,0,1,0";
+				else if(str.contains("Sony Computer Entertainment"))
+					str += ",0,1,0,0";
+				else if(str.contains("Capcom"))
+					str += ",1,0,0,0";
+				
+				String[] p = new String[]{"PS4", "XOne", "WiiU", "3DS", "PC", "PSV"};
+				
+				if(str.contains(p[0]))
+					str += ",0,0,0,0,0,0";
+				else if(str.contains(p[1]))
+					str += ",0,0,0,0,1,0";
+				else if(str.contains(p[2]))
+					str += ",0,0,0,1,0,0";
+				else if(str.contains(p[3]))
+					str += ",0,0,1,0,0,0";
+				else if(str.contains(p[4]))
+					str += ",0,1,0,0,0,0";
+				else if(str.contains(p[5]))
+					str += ",1,0,0,0,0,0";
+				
+				pw.println(str);
 			}
 			
 			sc.close();
